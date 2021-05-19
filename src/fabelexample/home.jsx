31d602d6c170c706fb523b1dc2b7a5e6a1ebc0e3
Jake Mirra <jpluto909@gmail.com>
2021-05-19 13:52:26 -0500

    Example complete.

--- src/fabelexample/home.jsx
+++ src/fabelexample/home.jsx
@@ -2,14 +2,14 @@ import { React, ReactDOM, useEffect, useState } from "react.mjs";
 
 function Home() {
 
-  let [count, setCount] = useState(0);
-  
+  const [count, setCount] = useState(0);
+
   return (
-    <Home>
+    <div>
       <div>Hello world in a component.</div>
       <div>You clicked {count} times.</div>
       <button onClick={() => setCount(count + 1)}>Click me.</button>
-    </Home>
+    </div>
   );
 }
 
